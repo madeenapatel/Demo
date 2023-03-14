af5611a7fcab28fe6d5885eb691fc13147813432
madeena <mpatel@genixventures.com>
2023-03-14 22:39:47 +1100

    My first Commit

---
+++ src/SelIntroduction.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SelIntroduction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\parag\\Downloads\\Softwares\\chromedriver_win32");
+		WebDriver driver = new ChromeDriver();
+	}
+
+}
